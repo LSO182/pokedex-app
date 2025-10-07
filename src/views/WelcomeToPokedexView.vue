f8a8d2e700facf5eab02c6d0734ff8a831d857b4
LSO182 <sanchezolmedoluciano@gmail.com>
2025-10-07 15:13:00 -0300

    first commit

---
+++ src/views/WelcomeToPokedexView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <h1>Poke</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
