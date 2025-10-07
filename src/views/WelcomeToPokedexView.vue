d2e045bfa0635ba3fd162c13de5246d9597e5caa
LSO182 <sanchezolmedoluciano@gmail.com>
2025-10-07 17:18:27 -0300

    welcome view advance plus barrel css files and structure

--- src/views/WelcomeToPokedexView.vue
+++ src/views/WelcomeToPokedexView.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
+  <div class="bg-primary-grey min-vh-100 pt-5">
     <div class="container">
-      <div class="row">
-        <h1>Poke</h1>
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-10 d-flex justify-content-center">
+          <div class="greet-pika-container position-relative">
+            <img
+              src="/src/assets/images/pikachu-welcome-img.png"
+              class="position-absolute greet-pika"
+              alt="Pikachu waving hello"
+            />
+          </div>
+        </div>
+        <div class="col-lg-10">
+          <h1 class="text-primary-black text-center">Welcome to Pokédex</h1>
+          <p class="text-secondary-grey text-center">
+            The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in
+            the Pokémon world.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -10,4 +25,16 @@
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.greet-pika-container {
+  width: 206px;
+  height: 206px;
+  background: url('/src/assets/images/bg-pokeball.png') no-repeat center/contain;
+}
+.greet-pika {
+  object-fit: cover;
+  top: -59px;
+  left: -43px;
+  height: 263px;
+}
+</style>
