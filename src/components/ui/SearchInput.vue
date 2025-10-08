04fb6309a8977a352f46db7d74573c5ba43be97e
LSO182 <sanchezolmedoluciano@gmail.com>
2025-10-08 20:44:21 -0300

    input serach and remove junk

---
+++ src/components/ui/SearchInput.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  modelValue?: string
+  placeholder?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const value = computed({
+  get: () => props.modelValue ?? '',
+  set: (v: string) => emit('update:modelValue', v),
+})
+</script>
+
+<template>
+  <div class="search-input position-relative">
+    <input
+      type="search"
+      class="form-control"
+      v-model="value"
+      :placeholder="placeholder ?? 'Search'"
+      aria-label="Search"
+    />
+  </div>
+</template>
+
+<style scoped>
+.search-input {
+  padding-top: 35px;
+  padding-bottom: 40px;
+}
+
+.search-input::before {
+  content: '';
+  position: absolute;
+  left: 15px;
+  right: 10px;
+  top: 47%;
+  transform: translateY(-50%);
+  width: 18px;
+  height: 18px;
+  background: url('/src/assets/images/Vector.png') no-repeat center/contain;
+  pointer-events: none;
+}
+
+.search-input > input.form-control {
+  width: 315px;
+  height: 50px;
+  padding-left: 42px;
+  box-shadow: 0px 2px 10px 0px #0000000a;
+  border-radius: 5px;
+  border: none;
+}
+</style>
--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -4,6 +4,7 @@ import { usePokemonStore } from '@/stores/pokemon'
 import { onMounted, ref, computed } from 'vue'
 import LoadingView from './LoadingView.vue'
 import PokemonCard from '@/components/PokemonCard.vue'
+import SearchInput from '@/components/ui/SearchInput.vue'
 
 const store = usePokemonStore()
 const { pokemons, loading, error } = storeToRefs(store)
@@ -28,6 +29,7 @@ const showLoading = computed(() => loading.value || !minDelayPassed.value)
           <p v-if="error" class="text-danger">{{ error }}</p>
           <div v-else>
             <div class="d-flex flex-column align-items-center justify-content-center">
+              <SearchInput />
               <div v-for="pokemon in pokemons" :key="pokemon.name">
                 <PokemonCard :name="pokemon.name" />
               </div>
